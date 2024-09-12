407293c2451d8cfda7f3115fa08cd3eb6c2d6698
Mukasa Matthew <matthewmukasa0@gmail.com>
2024-09-12 17:39:11 +0300

    Add files via upload

---
+++ firstprogram.js
@@ -0,0 +1,11 @@
+// ths is my first application
+
+    console.log("hello world")
+
+
+
+    console.log("B29053;")
+    console.log("Mukasa Matthew;")
+    console.log("BSIT")
+
+
---
+++ variables.js
@@ -0,0 +1,23 @@
+//declare a variable
+var height;
+height = 3.4;
+console.log(height)
+
+
+//storing area
+let area = 90.5;
+console.log(area)
+
+
+//storing name
+f_name = "Matthew";
+console.log(f_name)
+
+
+
+//initializing a variable
+const pie = 3.14;
+console.log(pie)
+
+//printing both name and height on the sameline
+console.log(f_name, " - ", height)
