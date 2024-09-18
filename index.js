d961d992620568069b787872740f1acfc95f47a9
Mukasa Matthew <matthewmukasa0@gmail.com>
2024-09-18 15:09:09 +0100

    //index updates

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello, world");
+let first_name = "jane"
+let last_name = "doe"
+
+const pie = 3.12344
+const max_speed =  4000
+
+var school_name = "UCU"
+
+
+//one line comments
+/*
+//multiline comment
+*/
